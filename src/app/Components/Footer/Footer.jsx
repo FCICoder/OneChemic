f0ea526c2fc4e9d092400d6960b8d6a37dc0cf51
Hamza-mohamed <hamzamohamed20201@gmail.com>
2023-12-30 20:54:50 +0200

    footer.

---
+++ src/app/Components/Footer/Footer.jsx
@@ -0,0 +1,121 @@
+'use client';
+import Image from "next/image";
+import React from "react";
+import fStyle from "./footer.module.css";
+import logo from "../../../../assets/Logo.jpg";
+import Link from "next/link";
+
+const Footer = () => {
+
+  return (
+    <div style={{ backgroundColor: "#1f44ab" }}>
+      <div className="container-fluid d-lg-flex d-block px-5 py-2">
+        <div className="  d-flex m-auto justify-content-center me-2">
+          <div className="">
+            <Image
+              src={logo.src}
+              width={80}
+              height={80}
+              className="rounded-circle img-fluid"
+              alt="one Chemic logo"
+            ></Image>
+          </div>
+          <div className="position-relative">
+            <div className={`${fStyle.titleLogo} ms-3 p-1 my-auto`}>
+              <p className={`${fStyle.responsiveLink}`}>OneChemic</p>
+              <p className={`${fStyle.responsiveLink}`}> since 2012</p>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-2 p-0 w-100" id="footerContent">
+          <div className="col-md-4 col-6 ">
+            <h1 className={`${fStyle.responsiveText}`}>Discover</h1>
+            <ul className="list-unstyled">
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  About us
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  Contact us
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  OneChemic
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-4 col-6 ">
+            <h1 className={`${fStyle.responsiveText}`}>our Products</h1>
+            <ul className="list-unstyled">
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  About us
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  Contact us
+                </Link>
+              </li>
+
+            </ul>
+          </div>
+          <div className="col-md-4  col-6 ">
+            <h1 className={`${fStyle.responsiveText}`}>headquarters</h1>
+            <ul className="list-unstyled">
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  About us
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  Contact us
+                </Link>
+              </li>
+              <li>
+                <Link href="#" className={`${fStyle.responsiveLink}`}>
+                  OneChemic
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="d-md-flex justify-content-between px-5 ">
+        <div>
+          <span className={`${fStyle.responsiveLink} text-dark `} style={{fontSize:'18px'}}>© 2024 OneChemic</span>
+        </div>
+        <div className="flex-end" onClick={()=>{
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+
+        }}>
+        <i class="fa-solid fa-circle-arrow-up fs-4 px-1"></i>
+          <span className={`${fStyle.responsiveLink} fs-6`}>Back to top</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import { imageOptimizer } from "next/dist/server/image-optimizer";
 import styles from "./page.module.css";
 import Content from "./Home/page.jsx";
+import ContactLink from "./Components/Contact-link/Contact-link";
+import Footer from "./Components/Footer/Footer";
 export default function Home() {
   return (
     <>
-      <Content/>
+      {/* <Content/> */}
+      <ContactLink/>
+      <Footer/>
     </>
   );
 }
