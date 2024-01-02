1649d30a393af48382246b44b2c262751598900f
Hamza-mohamed <hamzamohamed20201@gmail.com>
2024-01-02 20:26:54 +0200

    about ...

---
+++ app/[lng]/Category/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import products from "./products.json";
+import { useRouter } from "next/router";
+
+const Page = ({ params: { lng } }) => {
+  const [category, setCategory] = useState();
+  const myProducts = products;
+  useEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const categoryParam = urlSearchParams.get("category");
+    if (categoryParam) {
+      setCategory(categoryParam);
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <h1>{category}</h1>
+          {myProducts[category] &&
+            myProducts[category].map((item, index) => (
+              <div key={index} className="col-md-4 mb-4">
+                <div className="card">
+                  <div className="card-body">
+                    {lng === "en" ? (
+                      <>
+                        <h5 className="card-title">{item.en.title}</h5>
+                        <p className="card-text">{item.en.description}</p>
+                      </>
+                    ):(
+                      <>
+                        <h5 className="card-title">{item.ar.title}</h5>
+                        <p className="card-text">{item.ar.description}</p>
+                      </>
+                    )}
+                    {/* Add an image if there is a URL */}
+                    {item.ImageUrl && (
+                      <img
+                        src={item.ImageUrl}
+                        className="img-fluid"
+                        alt="Card"
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Page;
--- app/[lng]/Components/Navbar/navbar.jsx
+++ app/[lng]/Components/Navbar/navbar.jsx
@@ -7,8 +7,9 @@ import img from "../../../../assets/OneChmeicLogo.jpg";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
-export  const Navbar =  ({lng}) => {
-var myLang=lng
+export const Navbar = ({ lng }) => {
+  const hamza="jjjjjjjjjjjjjjjjjjj"
+  var myLang = lng;
 
   const router = usePathname();
   return (
@@ -34,61 +35,67 @@ var myLang=lng
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="   collapse navbar-collapse" id="collapsibleNavId">
-              <div className=" w-100 d-flex justify-content-center"> 
-
-              <ul className={` navbar-nav  mt-2 mt-lg-0 translate-50`}>
-                <li className="nav-item">
-                  <Link
-                    className={`${router == `/${lng}` ? `${styles.active}` : ""} ${
-                      styles.nav_link
-                    }  `}
-                    href={`/${lng}`}
-                  >
-                   {(myLang=='en')?"Home":"الصفحة الرئيسيه"}
-                   {/* {t('navbar.part1')} */}
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className={`${
-                      router == `/${lng}/about-us` ? `${styles.active}` : ""
-                    } ${styles.nav_link}`}
-                    href={`/${lng}/about-us`}
-                  >
-                    {(myLang=='en')?"About Us":"نبذة عنا"}
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className={`${
-                      router == `/${lng}/Contactus` ? `${styles.active}` : ""
-                    } ${styles.nav_link}`}
-                    href={`/${lng}/Contactus`}
-                  >
-                    {(myLang=='en')?"Contactus":"تواصل معنا"}
-                  </Link>
-                </li>{" "}
-                <li className="nav-item dropdown">
-                  <Link
-                    className={`${styles.nav_link} dropdown-toggle`}
-                    href="#"
-                    id="dropdownId"
-                    data-bs-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </Link>
-                  <div className="dropdown-menu" aria-labelledby="dropdownId">
-                    <Link className="dropdown-item" href="#">
-                      Action 1
+              <div className=" w-100 d-flex justify-content-center">
+                <ul className={` navbar-nav  mt-2 mt-lg-0 translate-50`}>
+                  <li className="nav-item">
+                    <Link
+                      className={`${
+                        router == `/${lng}` ? `${styles.active}` : ""
+                      } ${styles.nav_link}  `}
+                      href={`/${lng}`}
+                    >
+                      {myLang == "en" ? "Home" : "الصفحة الرئيسيه"}
+                      {/* {t('navbar.part1')} */}
                     </Link>
-                    <Link className="dropdown-item" href="#">
-                      Action 2
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`${
+                        router == `/${lng}/about-us` ? `${styles.active}` : ""
+                      } ${styles.nav_link}`}
+                      href={`/${lng}/about-us`}
+                    >
+                      {myLang == "en" ? "About Us" : "نبذة عنا"}
                     </Link>
-                  </div>
-                </li>
-              </ul>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      className={`${
+                        router == `/${lng}/Contactus` ? `${styles.active}` : ""
+                      } ${styles.nav_link}`}
+                      href={`/${lng}/Contactus`}
+                    >
+                      {myLang == "en" ? "Contactus" : "تواصل معنا"}
+                    </Link>
+                  </li>{" "}
+                  <li className="nav-item dropdown">
+                    <Link
+                      className={`${styles.nav_link} dropdown-toggle`}
+                      href="#"
+                      id="dropdownId"
+                      data-bs-toggle="dropdown"
+                      aria-haspopup="true"
+                      aria-expanded="false"
+                    >
+                      Dropdown
+                    </Link>
+                    <div className="dropdown-menu" aria-labelledby="dropdownId">
+                      <Link 
+                        className="dropdown-item"
+                        href={{
+                          pathname: `/${lng}/Category`,
+                          query: { category: "solevants" },
+                        }}
+                      >
+                        Action 1
+                        {/* {router.query=hamza} */}
+                      </Link>
+                      <Link className="dropdown-item" href="#">
+                        Action 2
+                      </Link>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
@@ -96,4 +103,4 @@ var myLang=lng
       </section>
     </>
   );
-}
+};
