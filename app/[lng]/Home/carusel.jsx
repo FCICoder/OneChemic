7a65f8a947972494fb726addb5c005117d93fcfa
FCICoder <https://github.com/FCICoder>
2024-01-11 12:07:57 +0200

    'carusel'

--- app/[lng]/Home/carusel.jsx
+++ app/[lng]/Home/carusel.jsx
@@ -7,7 +7,6 @@ import img1 from '../../../assets/slider/1.jpg'
 import img2 from '../../../assets/slider/2.jpg'
 import img3 from '../../../assets/slider/3.jpg'
 import img4 from '../../../assets/slider/4.jpg'
-import Image from 'next/image';
 import styles from './home.module.css'
 function SampleNextArrow(props) {
     const { className, style, onClick } = props;
@@ -42,29 +41,29 @@ function Carusel() {
         autoplaySpeed: 1800,
         initialSlide: 0,
         lazyLoad: true,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
+        // nextArrow: <SampleNextArrow />,
+        // prevArrow: <SamplePrevArrow />
 
     };
     return (
-        <div className="container" >
-            <Slider {...settings} className='m-auto  mt-4  ' style={{width:'84%'}} >
+        <div className="container-fluid overflow-hidden"  >
+            <Slider {...settings} className='m-auto  mt-4  ' style={{width:'100%'}} >
                 <div className={`${styles.image}  text-center `}>
 
-                    <img src={img1.src} alt='One chemic slider image 1' className='m-auto img-fluid ' style={{height:'400px',width:'90%',borderRadius:'20px'}}  />
+                    <img src={img1.src} alt='One chemic slider image 1' className='m-auto img-fluid ' style={{height:'400px',width:'100%'}}  />
 
                 </div>
 
                 <div className={`${styles.image}  text-center`}>
-                    <img src={img2.src} alt='One chemic slider image 2' className=' m-auto img-fluid ' style={{height:'400px',width:'90%',borderRadius:'20px'}}  />
+                    <img src={img2.src} alt='One chemic slider image 2' className=' m-auto img-fluid ' style={{height:'400px',width:'100%'}}  />
                 </div>
 
                 <div className={`${styles.image}  text-center`}>
-                    <img src={img3.src} alt='One chemic slider image 3' className=' m-auto img-fluid ' style={{height:'400px',width:'90%',borderRadius:'20px'}}  />
+                    <img src={img3.src} alt='One chemic slider image 3' className=' m-auto img-fluid ' style={{height:'400px',width:'100%'}}  />
                 </div>
 
                 <div className={`${styles.image}  text-center`}>
-                    <img src={img4.src} alt='One chemic slider image 4' className=' m-auto img-fluid ' style={{height:'400px',width:'90%',borderRadius:'20px'}}  />
+                    <img src={img4.src} alt='One chemic slider image 4' className=' m-auto img-fluid ' style={{height:'400px',width:'100%'}}  />
                 </div>
 
             </Slider>
