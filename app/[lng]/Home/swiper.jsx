92c378245b0d6591f606f7f4ff67405b262014a0
FCICoder <mahmoud.aadel1998@gmail.com>
2024-01-25 09:45:16 +0200

    oo

--- app/[lng]/Home/swiper.jsx
+++ app/[lng]/Home/swiper.jsx
@@ -1,104 +1,160 @@
-'use client'
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from 'react-slick';
-import img1 from '../../../assets/swiper/1.png'
-import img2 from '../../../assets/swiper/2.png'
-import img3 from '../../../assets/swiper/3.png'
-import img4 from '../../../assets/swiper/4.png'
-import img5 from '../../../assets/swiper/5.png'
-import styles from './home.module.css'
+import Slider from "react-slick";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import img1 from "../../../assets/swiper/1.png";
+import img2 from "../../../assets/swiper/2.png";
+import img3 from "../../../assets/swiper/3.png";
+import img4 from "../../../assets/swiper/4.png";
+import img5 from "../../../assets/swiper/5.png";
+import styles from "./home.module.css";
+import Image from "next/image";
 function Swiper() {
-    function SampleNextArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-            <div
-                className={className}
-                style={{ ...style, display: "block", background: "black", borderRadius: '10px', }}
-                onClick={onClick}
-            />
-        );
-    }
-    
-    function SamplePrevArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-            <div
-                className={className}
-                style={{ ...style, display: "block", background: "black", borderRadius: '10px' }}
-                onClick={onClick}
-            />
-        );
-    }
-    
+  const [sliderRef, setSliderRef] = useState(null);
+  const Cards = [
+    {
+      imageSrc: img1,
+      title: "oneChemic",
+      description: "One chemic swiper partners image 1",
+    },
+    {
+      imageSrc: img2,
+      title: "oneChemic",
+      description: "One chemic swiper partners image 2",
+    },
+    {
+      imageSrc: img3,
+      title: "oneChemic",
+      description: "One chemic swiper partners image 3",
+    },
+    {
+      imageSrc: img4,
+      title: "oneChemic",
+      description: "One chemic swiper partners image 4",
+    },
+    {
+      imageSrc: img5,
+      title: "oneChemic",
+      description: "One chemic swiper partners image 5",
+    },
+  ];
 
-        const settings = {
-            dots: false,
-            infinite: true,
-            speed: 700,
-            slidesToShow: 4,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            initialSlide: 0,
-            lazyLoad: true,
-            nextArrow: <SampleNextArrow />,
-            prevArrow: <SamplePrevArrow />,
-            responsive: [
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 4,
-          
-          
-                  }
-                },
-                {
-                  breakpoint: 600,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2,
-                    initialSlide: 2
-                  }
-                },
-                {
-                  breakpoint: 480,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
-        };
-        return (
-            <div className="container mt-5 overflow-hidden">
-          <h2 className=' text-center fst-italic fw-bold '>OUR PARTNERS</h2>
-                <Slider {...settings} className='m-auto  mt-4  ' style={{width:'87%'}} >
-                    <div className={`${styles.partners}  text-center `}>
-                        <img src={img1.src} alt='One chemic swiper partners image 1' className='m-auto img-fluid '   />
-                    </div>
-    
-                    <div className={`${styles.partners}  text-center`}>
-                        <img src={img2.src} alt='One chemic swiper partners image 2' className=' m-auto img-fluid '   />
-                    </div>
-    
-                    <div className={`${styles.partners}  text-center`}>
-                        <img src={img3.src} alt='One chemic swiper partners image 3' className=' m-auto img-fluid '   />
-                    </div>
-    
-                    <div className={`${styles.partners}  text-center`}>
-                        <img src={img4.src} alt='One chemic swiper partners image 4' className=' m-auto img-fluid '   />
-                    </div>
-    
-                    <div className={`${styles.partners}  text-center`}>
-                        <img src={img5.src} alt='One chemic swiper partners image 5' className=' m-auto img-fluid '   />
-                    </div>
-                </Slider>
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    initialSlide: 0,
+    lazyLoad: false,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="container mt-5 overflowX-hidden position-relative">
+      <h2 className=" text-center fst-italic fw-bold ">OUR PARTNERS</h2>
+      <Slider
+        ref={(slider) => setSliderRef(slider)}
+        className={`m-auto  mt-4  ${styles.slider}`}
+        {...settings}
+        style={{ width: "87%" }}
+      >
+        {Cards.map((item, index) => {
+          return (
+            <div className={`${styles.partners}  text-center `} key={index}>
+              <Image
+                src={item.imageSrc}
+                alt={item.description}
+                width="100%"
+                height="100%"
+                className="m-auto img-fluid h-100"
+                priority={true}
+              />
             </div>
-    
-        )
+          );
+        })}
+        {/* <div className={`${styles.partners}  text-center `}>
+          <img
+            src={img1.src}
+            alt="One chemic swiper partners image 1"
+            className="m-auto img-fluid "
+          />
+        </div>
+
+        <div className={`${styles.partners}  text-center`}>
+          <img
+            src={img2.src}
+            alt="One chemic swiper partners image 2"
+            className=" m-auto img-fluid "
+          />
+        </div>
+
+        <div className={`${styles.partners}  text-center`}>
+          <img
+            src={img3.src}
+            alt="One chemic swiper partners image 3"
+            className=" m-auto img-fluid "
+          />
+        </div>
+
+        <div className={`${styles.partners}  text-center`}>
+          <img
+            src={img4.src}
+            alt="One chemic swiper partners image 4"
+            className=" m-auto img-fluid "
+          />
+        </div>
+
+        <div className={`${styles.partners}  text-center`}>
+          <img
+            src={img5.src}
+            alt="One chemic swiper partners image 5"
+            className=" m-auto img-fluid "
+          />
+        </div> */}
+      </Slider>
+
+      <button
+        onClick={() => sliderRef.slickNext()}
+        className={` ${styles.nextbtnP}`}
+      >
+        <FaChevronRight />
+      </button>
+      <button
+        onClick={() => sliderRef.slickPrev()}
+        className={`btn  ${styles.prevbtnP}`}
+      >
+        
+        <FaChevronLeft />
+      </button>
+    </div>
+  );
 }
 
-export default Swiper
+export default Swiper;
